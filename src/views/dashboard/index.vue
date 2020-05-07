06c67932952182709febcc141d9e31b62b44b76c
李跃平 <yueping.li@capgemini.com>
2020-05-07 17:25:31 +0800

    add home bg

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>
-    dashboard--index
-  </div>
+  <div class="dashboard" :style="{backgroundImage: bgImg}"></div>
 </template>
+<script>
+import BGImg from '@/assets/home.jpg'
+export default {
+  name: 'HomeIndex',
+  data () {
+    return {
+      bgImg: 'url(' + BGImg + ')'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.dashboard {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  background-position: center;
+  background-size: cover;
+}
+</style>
